18c51666a1b9e771b8472f90b2f84c25e3ed5c27
James Alan Bush <jamesalanbush1972@gmail.com>
2024-05-25 17:58:49 -0400

    Initial Commit

---
+++ TypographyStyleGuideLayout/TypographyStyleGuideLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypographyStyleGuideLayoutApp.swift
+//  TypographyStyleGuideLayout
+//
+//  Created by Xcode Developer on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct TypographyStyleGuideLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
